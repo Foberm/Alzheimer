b5f2df84d4a8b649d8e825c2ffc7209a2607649a
Max <max.woedl@gmail.com>
2017-03-02 23:27:20 +0100

    Moved pictures to Start, Comments added

--- app/src/main/java/com/example/maxim/alzheimer/QuizPage.java
+++ app/src/main/java/com/example/maxim/alzheimer/QuizPage.java
@@ -15,25 +15,27 @@ import java.util.Random;
 
 public class QuizPage extends AppCompatActivity {
 
-    public List<String> pictures = new ArrayList<String>();
-    public List<String[]> answers = new ArrayList<String[]>();
-    public List<Integer> used = new ArrayList<Integer>();
-    String searchedWord = "";
-    boolean isRated = false;
-    long time = 0;
     ImageButton btn_ans1, btn_ans2;
     TextView lbl_searchedWord;
-    int trueButton=0;
+
+    public List<String[]> answers = new ArrayList<String[]>();
+    public List<Integer> used = new ArrayList<Integer>();
+
+    boolean isRated = false;
+    long time = 0;
+
+    int searchedButton = 0;
+    String searchedWord = "";
 
     Runnable runnable = new Runnable() {
         public void run() {
-            String sW=searchedWord;
+            String sW = searchedWord;
             synchronized (this) {
                 try {
                     wait(StartPage.secondsPerQuestion * 1000);
                     if(sW.equals(searchedWord))writeToAnswers(0);
-                } catch (Exception e) {
                 }
+                catch (Exception e) {}
             }
         }
     };
@@ -42,26 +44,21 @@ public class QuizPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_page);
+
         btn_ans1 = (ImageButton) findViewById(R.id.btn_answer1);
         btn_ans2 = (ImageButton) findViewById(R.id.btn_answer2);
         lbl_searchedWord = (TextView) findViewById(R.id.label_word);
-
-        final Field[] fields = R.drawable.class.getDeclaredFields();
-        String regex = "[a-z]+";
-        for (int i = 0; i < fields.length; i++) {
-            if (fields[i].getName().matches(regex))
-                pictures.add(fields[i].getName());
-        }
 
         /*
         String photoPath = Environment.getExternalStorageDirectory()+ "/" + StartPage.main_directory + "/Almost.jpg";
         Drawable d = new BitmapDrawable(getResources(), BitmapFactory.decodeFile(photoPath));
         btn_ans1.setImageDrawable(d);
         */
+
         findViewById(R.id.btn_answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(trueButton==1)writeToAnswers(1);
+                if(searchedButton ==1)writeToAnswers(1);
                 else writeToAnswers(2);
             }
         });
@@ -69,7 +66,7 @@ public class QuizPage extends AppCompatActivity {
         findViewById(R.id.btn_answer2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(trueButton==2)writeToAnswers(1);
+                if(searchedButton ==2)writeToAnswers(1);
                 else writeToAnswers(2);
             }
         });
@@ -79,33 +76,33 @@ public class QuizPage extends AppCompatActivity {
 
 
     public void writeToAnswers(int state) {
-        String[] temp = new String[3];
-        temp[0] = searchedWord;
+        String[] tmp = new String[3];
+        tmp[0] = searchedWord;
         switch (state) {
             case -1:    //beenden
                 while (answers.size()<StartPage.numberOfQuestions-1){
                     String[] tmp2 ={"NB","NB","0"};
                     answers.add(tmp2);
                 }
-                temp[1] = "NB";
-                temp[2] = "" + 0;
+                tmp[1] = "NB";
+                tmp[2] = "" + 0;
                 break;
             case 0:     //skippen
-                temp[1] = "NB";
-                temp[2] = "" + 0;
+                tmp[1] = "NB";
+                tmp[2] = "" + 0;
                 break;
             case 1:     //richtig
                 Log.d("switch", "richtig");
-                temp[1] = "Richtig";
-                temp[2] = "" + (System.currentTimeMillis() - time / 1000);
+                tmp[1] = "Richtig";
+                tmp[2] = "" + (System.currentTimeMillis() - time / 1000);
                 break;
             case 2:     //falsch
                 Log.d("switch", "falsch");
-                temp[1] = "Falsch";
-                temp[2] = "" + (System.currentTimeMillis() - time / 1000);
+                tmp[1] = "Falsch";
+                tmp[2] = "" + (System.currentTimeMillis() - time / 1000);
                 break;
         }
-        answers.add(temp);
+        answers.add(tmp);
 
         //Tutorial
         if (answers.size() == StartPage.numberOfTutorials && !isRated) {
@@ -113,7 +110,7 @@ public class QuizPage extends AppCompatActivity {
             isRated = true;
         }
         if(answers.size() == StartPage.numberOfQuestions && isRated){
-            lbl_searchedWord.setText("Game Ended");
+            lbl_searchedWord.setText("Fragenanzahl erreicht");
             mythread.interrupt();
         }
         else newWord();
@@ -123,38 +120,39 @@ public class QuizPage extends AppCompatActivity {
     Thread mythread = new Thread(runnable);
     public void newWord() {
         mythread.interrupt();
+
         Random rand = new Random();
         int pic1 = 0;
         do {
-            pic1 = rand.nextInt(pictures.size());
+            pic1 = rand.nextInt(StartPage.pictures.size());
         } while (used.contains(pic1));
 
         int pic2 = 0;
         do {
-            pic2 = rand.nextInt(pictures.size());
+            pic2 = rand.nextInt(StartPage.pictures.size());
         } while (used.contains(pic2) || pic1 == pic2);
 
         btn_ans1 = (ImageButton) findViewById(R.id.btn_answer1);
         btn_ans2 = (ImageButton) findViewById(R.id.btn_answer2);
         Context contextAns1 = btn_ans1.getContext();
-        int idAns1 = contextAns1.getResources().getIdentifier(pictures.get(pic1), "drawable", contextAns1.getPackageName());
+        int idAns1 = contextAns1.getResources().getIdentifier(StartPage.pictures.get(pic1), "drawable", contextAns1.getPackageName());
         btn_ans1.setImageResource(idAns1);
         Context contextAns2 = btn_ans2.getContext();
-        int idAns2 = contextAns1.getResources().getIdentifier(pictures.get(pic2), "drawable", contextAns2.getPackageName());
+        int idAns2 = contextAns1.getResources().getIdentifier(StartPage.pictures.get(pic2), "drawable", contextAns2.getPackageName());
         btn_ans2.setImageResource(idAns2);
 
 
         boolean correctAnswer = rand.nextBoolean();
         if (correctAnswer) {
-            lbl_searchedWord.setText("" + pictures.get(pic1).toUpperCase());
-            searchedWord = pictures.get(pic1);
-            trueButton=1;
+            lbl_searchedWord.setText("" + StartPage.pictures.get(pic1).toUpperCase());
+            searchedWord = StartPage.pictures.get(pic1);
+            searchedButton = 1;
             used.add(pic1);
         } else {
-            lbl_searchedWord.setText("" + pictures.get(pic2).toUpperCase());
-            searchedWord = pictures.get(pic2);
+            lbl_searchedWord.setText("" + StartPage.pictures.get(pic2).toUpperCase());
+            searchedWord = StartPage.pictures.get(pic2);
             used.add(pic2);
-            trueButton=2;
+            searchedButton = 2;
         }
         mythread = new Thread(runnable);
         mythread.start();
--- app/src/main/java/com/example/maxim/alzheimer/StartPage.java
+++ app/src/main/java/com/example/maxim/alzheimer/StartPage.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StartPage extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class StartPage extends AppCompatActivity {
     public static int secondsPerQuestion = 15;
     public static int numberOfTutorials = 1;
     public static String main_directory = "AlzheimerTestOrdner";
+    public static List<String> pictures = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +33,28 @@ public class StartPage extends AppCompatActivity {
             f.mkdirs();
         }
 
+        //Scanning the available pictures
+        final Field[] fields = R.drawable.class.getDeclaredFields();
+        String regex = "[a-z]+";
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i].getName().matches(regex))
+                pictures.add(fields[i].getName());
+        }
+
+        //Let User know the maximum amount of Questions possible
+        ((EditText)findViewById(R.id.numOfQuestionsInput)).setHint("Anzahl Fragen.. (Standard: 10, Maximal: " + (pictures.size()-1) + ")");
+
         findViewById(R.id.beginnen).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 username = ((EditText)findViewById(R.id.numOfQuestionsInput)).getText().toString();
                 String anzahl = ((EditText)findViewById(R.id.numOfQuestionsInput)).getText().toString();
                 String zeit = ((EditText)findViewById(R.id.timePerQuestionInput)).getText().toString();
-                if(!anzahl.isEmpty())
-                    numberOfQuestions = Integer.parseInt(anzahl);
+                if(!anzahl.isEmpty()) {
+                    int a = Integer.parseInt(anzahl);
+                    if(a > pictures.size()-1)
+                        numberOfQuestions = a;
+                }
                 if(!zeit.isEmpty())
                     secondsPerQuestion = Integer.parseInt(zeit);
                 startActivity(new Intent(StartPage.this, QuizPage.class));
